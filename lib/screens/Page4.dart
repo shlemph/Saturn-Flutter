b15ff069b10e6d372ff00969542ecba47b220bbe
AIDXNZ <asmaidan@outlook.com>
2018-06-07 16:27:28 -0500

    Dio

--- lib/screens/Page4.dart
+++ lib/screens/Page4.dart
@@ -4,62 +4,66 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'dart:convert' as convert;
 import 'package:flutter_fetch_widget/flutter_fetch_widget.dart';
-
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Four extends StatelessWidget {
+  final storage = new FlutterSecureStorage();
+
+  
+  
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      bottomNavigationBar: new BottomAppBar(
-        hasNotch: true,
-        color: new Color(0xFF6200ea),
-        child: new Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            BackButton(
-              color: Colors.white,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-          onPressed: () => {},
-          elevation: 5.0,
-          notchMargin: 7.0,
-          highlightElevation: 1.0,
-          backgroundColor: Colors.black,
-          child: Icon(Icons.add),
-      ), floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-        body: new Center(
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            new Container(
-              decoration: new BoxDecoration(
-                gradient: new LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end:  Alignment.bottomRight,
-                  colors: [
-                    new Color(0xFF6ff9ff),
-                    new Color(0xFF000070)
-                  ]
-                )
-              ),
-              child: new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                new Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
-                  child: new Material(
-                    borderRadius: new BorderRadius.circular(100.0),
-                    elevation: 5.0,
-                      child: new CachedNetworkImage(
-                        imageUrl: "https://avatars.io/instagram/a.i.d.x.n",
-                        width: 84.0, height: 84.0,
-                    ),
-                  ),
+        return new Scaffold(
+          bottomNavigationBar: new BottomAppBar(
+            hasNotch: true,
+            color: new Color(0xFF6200ea),
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                BackButton(
+                  color: Colors.white,
                 ),
-
+              ],
+            ),
+          ),
+          floatingActionButton: new FloatingActionButton(
+              onPressed: () => {},
+              elevation: 5.0,
+              notchMargin: 7.0,
+              highlightElevation: 1.0,
+              backgroundColor: Colors.black,
+              child: Icon(Icons.add),
+          ), floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+            body: new Center(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new Container(
+                  decoration: new BoxDecoration(
+                    gradient: new LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end:  Alignment.bottomRight,
+                      colors: [
+                        new Color(0xFF6ff9ff),
+                        new Color(0xFF000070)
+                      ]
+                    )
+                  ),
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                    new Padding(
+                      padding: const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+                      child: new Material(
+                        borderRadius: new BorderRadius.circular(100.0),
+                        elevation: 5.0,
+                          child: new CachedNetworkImage(
+                            imageUrl: "https://avatars.io/instagram/a.i.d.x.n",
+                            width: 84.0, height: 84.0,
+                        ),
+                      ),
+                    ),
+                    new Text("Aidan"),
                 new Padding(
                 padding: const EdgeInsets.fromLTRB(8.0, 15.0, 8.0, 8.0),
                 child: new FetchWidget<Balance>(
@@ -82,7 +86,10 @@ class Four extends StatelessWidget {
                           padding: const EdgeInsets.fromLTRB(350.0,0.0,0.0,0.0),
                           child: new IconButton(
                             color: Colors.white,
-                            onPressed: () => fetchPost.doFetch(),
+                            onPressed: () async{ fetchPost.doFetch();
+                            final user_id = await storage.read(key: 'user_id');
+                            print(user_id);
+                            },
                             icon: new Icon(Icons.refresh),
                           ),
                         )
--- lib/screens/ProfileForm.dart
+++ lib/screens/ProfileForm.dart
@@ -7,6 +7,8 @@ import 'package:stellaris/Homepage.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../main.dart';
+import 'dart:convert' as convert;
+import 'package:dio/dio.dart';
 
 class ProfileForm extends StatefulWidget {
   @override
@@ -14,23 +16,20 @@ class ProfileForm extends StatefulWidget {
 }
 
 class ProfileFormState extends State<ProfileForm> {
-  final textController = new TextEditingController();
+  final formKey = new GlobalKey<FormState>();
+
+  String _userName;
+  String _firstName;
+  String _lastName;
 
   @override
   void initState() {
     super.initState();
-
-    textController.addListener(listener);
   }
 
-  listener() {
-    print("${textController.text}");
-  }
-
-  authState() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool authState = (prefs.getBool('authState'));
-    await prefs.setBool('authState', true);
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   final storage = new FlutterSecureStorage();
@@ -45,46 +44,35 @@ class ProfileFormState extends State<ProfileForm> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               new Form(
-                child: new TextFormField(
-                  controller: textController,
-                  onSaved: (text) async =>
-                      await storage.write(key: "userName", value: text),
-                  initialValue: null,
-                  decoration: new InputDecoration(
-                      labelText: 'User Name',
-                      contentPadding: new EdgeInsets.all(12.0),
-                      border: InputBorder.none),
-                ),
-              ),
-              new Padding(
-                padding: const EdgeInsets.only(top: 5.0),
-                child: new Form(
-                  child: new TextFormField(
-                    controller: textController,
-                    onSaved: (text) async =>
-                        await storage.write(key: "firstName", value: text),
-                    initialValue: null,
-                    decoration: new InputDecoration(
-                        labelText: 'First Name',
-                        contentPadding: new EdgeInsets.all(12.0),
-                        border: InputBorder.none),
-                  ),
-                ),
-              ),
-              new Padding(
-                padding: const EdgeInsets.only(top: 5.0),
-                child: new Form(
-                  child: new TextFormField(
-                    controller: textController,
-                    onSaved: (text) async =>
-                        await storage.write(key: "lastName", value: text),
-                    initialValue: null,
-                    decoration: new InputDecoration(
-                      labelText: 'Last Name',
-                      contentPadding: new EdgeInsets.all(12.0),
-                      border: InputBorder.none,
+                key: formKey,
+                child: new Column(
+                  children: <Widget>[
+                    new TextFormField(
+                      onSaved: (val) => _userName = val,
+                      initialValue: null,
+                      decoration: new InputDecoration(
+                          labelText: 'User Name',
+                          contentPadding: new EdgeInsets.all(12.0),
+                          border: InputBorder.none),
                     ),
-                  ),
+                    new TextFormField(
+                      onSaved: (val) => _firstName = val,
+                      initialValue: null,
+                      decoration: new InputDecoration(
+                          labelText: 'First Name',
+                          contentPadding: new EdgeInsets.all(12.0),
+                          border: InputBorder.none),
+                    ),
+                    new TextFormField(
+                      onSaved: (val) => _lastName = val, 
+                      initialValue: null,
+                      decoration: new InputDecoration(
+                        labelText: 'Last Name',
+                        contentPadding: new EdgeInsets.all(12.0),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ],
                 ),
               ),
               new MaterialButton(
@@ -101,9 +89,33 @@ class ProfileFormState extends State<ProfileForm> {
     ));
   }
 
+  // Sets AuthState to true and Registers User
   void submit() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('authState', true);
-    print("User Signed In");
+    final form = formKey.currentState;
+    if(form.validate()){
+      form.save();
+      performLogin();
+      saveInfo();
+    }
   }
-}
+
+  void performLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final storage = new FlutterSecureStorage();
+    Dio dio = new Dio();
+    var url = "https://matrix.org/_matrix/client/r0/register";
+    Response response = await dio.post(url, data: {"username": "$_userName", "auth": {"type":"m.login.token"}});    
+    final json = convert.json.decode(response.data);
+    String access_token = json['access_token'];
+    prefs.setString("access_token", access_token);
+    storage.write(key: "access_token", value: "$access_token");
+    await prefs.setBool('authState', true);
+    print("User Signed In" + "${response.data}");
+  }
+
+  void saveInfo() {
+    final storage = new FlutterSecureStorage();
+    storage.write(key: "userName", value: "$_userName");
+    storage.write(key: "firstName", value: "$_firstName");
+    storage.write(key: "lastName", value: "$_lastName");
+  }
